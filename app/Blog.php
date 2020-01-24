befadb1d2edf4fcfb8e1144ae51315c155c42f66
ahmadskdev <asklko2004@gmail.com>
2020-01-24 10:20:05 +0530

    tags

--- app/Blog.php
+++ app/Blog.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    //
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
+
+    public function tags(){
+        return $this->belongsToMany('App\Tag');
+    }
 }
--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function blogs(){
+        return $this->belongsTo('App\Blog');
+    }
 }
